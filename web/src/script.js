da2e96179f3d992fc44c76518649d547a4751411
Antoine Torossian <torossian97@gmail.com>
2019-01-26 16:34:05 -0800

    session functions

--- web/src/script.js
+++ web/src/script.js
@@ -1,27 +1,48 @@
+function session_start(){
+	sessionStorage.doctor_id = "";
+	sessionStorage.patient_id = "";
+}
+
+function set_doctor(doctor){
+	sessionStorage.doctor_id = doctor;
+}
+
+function set_session(name, values){
+	sessionStorage.setItem(name, values);
+}
+
+function get_session(name){
+	return sessionStorage.getItem(name);
+}
+
 function createNode(element) {
       return document.createElement(element);
   }
 
-  function append(parent, el) {
+function append(parent, el) {
     return parent.appendChild(el);
   }
   
-  function json_load(url){
-	  fetch(url)
+function json_load(url){
+	  return new Promise((resolve, reject) => {
+		fetch(url)
 		  .then(function(response) {
-			return response.json();
+			return resolve(response.json());
 		  })
 		  .catch(function(error) {
 			console.log(error);
-		});
+			return reject(error);
+		});  
+	  })
   }
 
-  function patient_load(){
+  async function patient_load(){
+	  
 	  const ul = document.getElementById('authors');
 	  const url = 'https://randomuser.me/api/?results=10';
-	  authors = json_load(url);
-	  console.log(authors);
-		return authors.map(function(patient) {
+	  
+	  json_load(url).then( (resp) => {
+		return resp.results.map(function(patient) {
 		  let li = createNode('li'),
 			  img = createNode('img'),
 			  span = createNode('span');
@@ -33,6 +54,9 @@ function createNode(element) {
 		  append(li, span);
 		  append(document.getElementById('authors'), li);
 		});
+	  }
+	  
+	  )
   }
   
 	function prescription_load(){

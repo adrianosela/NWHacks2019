6368ba80636066218e3e7fba0b163c45961bf24e
Felipe <felipeballesteros1@gmail.com>
2019-01-27 01:33:09 -0800

    WIP

---
+++ iOS/NWHacks/PrescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrescriptionViewController.swift
+//  NWHacks
+//
+//  Created by Felipe on 2019-01-26.
+//  Copyright © 2019 Felipe. All rights reserved.
+//
+
+import UIKit
+
+class PrescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
